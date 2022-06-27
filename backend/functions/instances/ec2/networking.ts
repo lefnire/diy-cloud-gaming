f427066bc3f53f54e2c980af8bcef2a36a95acd2
Impiorum <brettrenelle1980@gmail.com>
2022-06-27 10:14:31 -0600

    home changes

--- backend/functions/instances/ec2/networking.ts
+++ backend/functions/instances/ec2/networking.ts
@@ -1,3 +1,72 @@
+import { EC2Client,
+  CreateVpcCommand,
+  CreateSubnetCommand,
+    DescribeAvailabilityZonesCommand,
+    CreateSecurityGroupCommand,
+    AuthorizeSecurityGroupIngressCommand,
+    AuthorizeSecurityGroupEgressCommand
+} from "@aws-sdk/client-ec2";
+
+const aws = new EC2Client({region: 'us-east-1'});
+async function sampleCode() {
+  const createdVpc = await aws.send(new CreateVpcCommand({
+    CidrBlock: "10.97.0.0/18"
+  }))
+  const {VpcId} = createdVpc.Vpc!
+
+  const azs = await aws.send(new DescribeAvailabilityZonesCommand({}))
+  const firstAvailableAz = azs.AvailabilityZones![0].ZoneId
+
+  const createdSubnet = await aws.send(new CreateSubnetCommand({
+    VpcId,
+    AvailabilityZone: firstAvailableAz,
+    CidrBlock: '10.97.0.0/24'
+  }))
+
+  const sg = await aws.send(new CreateSecurityGroupCommand({
+    VpcId,
+    GroupName: "Gaming Security Group",
+    Description: "Our Security Group",
+
+  }))
+  const {GroupId} = sg
+  const authEgress = await aws.send(new AuthorizeSecurityGroupEgressCommand({
+    GroupId,
+    CidrIp: "0.0.0.0/0",
+    FromPort: -1,
+    ToPort: -1
+
+  }))
+
+  const rules = [
+    {from: 38810, to: 38840, protocol: "TCP", name: "Virtual Deaktop VR"},
+    {from: 38810, to: 38840, protocol: "UDP", name: "Virtual Deaktop VR"},
+    {from: 8443, to: 8443, protocol: "TCP", name: "NiceDCV"},
+    {from: 8443, to: 8443, protocol: "UDP", name: "NiceDCV"},
+    {from: 8000, to: 8040, protocol: "TCP", name: "Parsec"},
+  ]
+
+  await Promise.all(rules.map(async function CreateRule(rule) {
+    const authIngress = await aws.send(new AuthorizeSecurityGroupIngressCommand({
+      GroupId,
+      CidrIp: "191.213.28.1/32",
+      FromPort: rule.from,
+      ToPort: rule.to,
+      IpProtocol: "rule.protocol,",
+      SourceSecurityGroupName: rule.name,
+
+    }))
+  }))
+}
+
+
+
+
+
+
+
+
+
 import {
   EC2Client,
   AcceptReservedInstancesExchangeQuoteCommand
